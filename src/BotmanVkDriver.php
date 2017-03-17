1b521944c96a83e4d7ddb142ba9f66c792cb538e
Vlady <vladronaldo7@gmail.com>
2017-03-18 01:55:44 +0200

    made method stubs will try to implement it

--- src/BotmanVkDriver.php
+++ src/BotmanVkDriver.php
@@ -1,15 +1,103 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ubuntu
- * Date: 3/18/17
- * Time: 1:31 AM
- */
 
 namespace BotmanVkDriver;
 
 
-class BotmanVkDriver
+use Mpociot\BotMan\Answer;
+use Mpociot\BotMan\Drivers\Driver;
+use Mpociot\BotMan\Interfaces\DriverInterface;
+use Mpociot\BotMan\Interfaces\UserInterface;
+use Mpociot\BotMan\Message;
+use Mpociot\BotMan\Question;
+use Symfony\Component\HttpFoundation\Request;
+
+class BotmanVkDriver extends Driver
 {
 
+    const DRIVER_NAME = 'BotFramework';
+
+    /**
+     * @param Request $request
+     * @return void
+     */
+    public function buildPayload(Request $request)
+    {
+
+    }
+
+    /**
+     * Determine if the request is for this driver.
+     *
+     * @return bool
+     */
+    public function matchesRequest()
+    {
+        // TODO: Implement matchesRequest() method.
+    }
+
+    /**
+     * Retrieve the chat message(s).
+     *
+     * @return array
+     */
+    public function getMessages()
+    {
+        // TODO: Implement getMessages() method.
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBot()
+    {
+        // TODO: Implement isBot() method.
+    }
+
+    /**
+     * @return bool
+     */
+    public function isConfigured()
+    {
+        // TODO: Implement isConfigured() method.
+    }
+
+    /**
+     * Retrieve User information.
+     * @param Message $matchingMessage
+     * @return UserInterface
+     */
+    public function getUser(Message $matchingMessage)
+    {
+        // TODO: Implement getUser() method.
+    }
+
+    /**
+     * @param Message $message
+     * @return Answer
+     */
+    public function getConversationAnswer(Message $message)
+    {
+        // TODO: Implement getConversationAnswer() method.
+    }
+
+    /**
+     * @param string|Question $message
+     * @param Message $matchingMessage
+     * @param array $additionalParameters
+     * @return $this
+     */
+    public function reply($message, $matchingMessage, $additionalParameters = [])
+    {
+        // TODO: Implement reply() method.
+    }
+
+    /**
+     * Return the driver name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return static::DRIVER_NAME;
+    }
 }
